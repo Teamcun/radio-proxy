ae37fc739e5adcff19436438730d06c21b39f44b
Teamcun <106079901+Teamcun@users.noreply.github.com>
2025-05-26 16:03:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ app.use(cors());
 
 const PORT = process.env.PORT || 10000;
 
-app.get('/radio', async (req, res) => {
+app.get('/', async (req, res) => {
   const radioUrl = 'https://uk2freenew.listen2myradio.com/live.mp3?typeportmount=s1_32167_stream_195287354';
 
   try {
-    const response = await fetch(radioUrl);
-    if (!response.ok) throw new Error('No se pudo acceder al stream');
+    const response = await fetch(radioUrl, {
+      headers: {
+        'Referer': 'https://ilcna.radiostream321.com/',
+        'Origin': 'https://ilcna.radiostream321.com/'
+      }
+    });
+    if (!response.ok) throw new Error('No se pudo acceder a la radio');
 
     res.set({
       'Content-Type': 'audio/mpeg',
